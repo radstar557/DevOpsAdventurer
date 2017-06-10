ae7b5fd5bc1df7c27bdcbc75d33f98548564ed0f
TrungHoang <trunghoang@admins-MacBook-2.local>
2017-06-10 11:38:10 +0200

    get rank

--- dplayer.py
+++ dplayer.py
@@ -9,7 +9,7 @@
 # Configuration
 # You need to change the setting according to your environment
 gregister_url='http://192.168.0.5:5001'
-glocalip_adr='192.168.0.33'
+glocalip_adr='192.168.0.12'
 
 # -----------------------------------------------------------
 
@@ -25,6 +25,89 @@ api = Api(app)
 
 # Web API to be called from the poker manager
 class PokerPlayerAPI(Resource):
+    global poker
+    def poker(hands):
+        "Return the best hand: poker([hand,...]) => hand"
+        return max(hands, key=hand_rank)
+
+    global hand_rank
+    def hand_rank(hand):
+        ranks = card_ranks(hand)
+        if straight(ranks) and flush(hand):  # straight flush
+            return (8, max(ranks))
+        elif kind(4, ranks):  # 4 of a kind
+            return (7, kind(4, ranks), kind(1, ranks))
+        elif kind(3, ranks) and kind(2, ranks):  # full house
+            return (6, kind(3, ranks), kind(2, ranks))
+        elif flush(hand):  # flush
+            return (5, ranks)
+        elif straight(ranks):  # straight
+            return (4, max(ranks))
+        elif kind(3, ranks):  # 3 of a kind
+            return (3, kind(3, ranks), ranks)
+        elif two_pair(ranks):  # 2 pair
+            return (2, two_pair(ranks), ranks)
+        elif kind(2, ranks):  # kind
+            return (1, kind(2, ranks), ranks)
+        else:  # high card
+            return (0, ranks)
+
+    global card_ranks
+    def card_ranks(cards):
+        "Return a list of the ranks,sorted with higher first"
+        ranks = ['--23456789TJQKA'.index(r) for r, s in cards]
+        ranks.sort(reverse=True)
+        return ranks
+
+    global straight
+    def straight(ranks):
+        "Return True if the ordered ranks from a 5-card straight"
+        return (max(ranks) - min(ranks) == 4) and len(set(ranks)) == 5
+
+    global flush
+    def flush(hand):
+        "Return True if all the cards have the same suit"
+        suits = [s for r, s in hand]
+        return len(set(suits)) == 1
+
+    global kind
+    def kind(n, ranks):
+        for r in ranks:
+            if ranks.count(r) == n: return r
+        return None
+
+    global two_pair
+    def two_pair(ranks):
+        pair = kind(2, ranks)
+        lowpair = kind(2, list(reversed(ranks)))
+        print(pair and lowpair)
+        if pair and lowpair != pair:
+            return (pair, lowpair)
+        else:
+            return None
+
+    global test
+    def test():
+        "Test cases for the functions in poker program"
+        sf = "6D 7C".split()  # Straight Flush
+        fk = "9D 9H 9S 9C 7D".split()  # Four of a Kind
+        fh = "TD TC TH 7C 7D".split()  # Full House
+        # result of card_ranks(sf) is [10,9,8,7,6]
+        # assert card_ranks(sf) == [10,9,8,7,6]
+        # assert card_ranks(fk) == [9,9,9,9,7]
+        # assert card_ranks(fh) == [10,10,10,7,7]
+        # print(poker([sf]))
+        print(hand_rank(fk))
+        # poker([sf, fk, fh]) is sf
+        # assert poker([sf, fk, fh]) == sf
+        # assert poker([fk, fh]) == fk
+        # assert poker([fh, fh]) == fh
+        # assert poker([sf]) == sf
+        # assert poker([sf] + 99*[fh]) == sf
+        # assert hand_rank(sf) == (8, 10)
+        # assert hand_rank(fk) == (7, 9, 7)
+        # assert hand_rank(fh) == (6, 10, 7)
+        return 'tests pass'
 
     ## return bid to caller
     #
@@ -66,7 +149,6 @@ class PokerPlayerAPI(Resource):
     # @return a dictionary containing the following values
     #         bid  : a number between 0 and max_bid
     def __get_bid(self, data):
-        print('Hello Adventurer')
         print(data)
         return 0
 
@@ -77,15 +159,17 @@ class PokerPlayerAPI(Resource):
         data = json.loads(data)
 
         if command_id == 'get_bid':
-            print('Inside get')
-            #print( data)
-            print('min_bid:', data[u'min_bid'])
-            print('pot:', data[u'pot'])
-            print('big_blind:', data[u'big_blind'])
-            print('hand:', data[u'hand'])
+
+            # print('Inside get')
+            # print('min_bid:', data[u'min_bid'])
+            # print('pot:', data[u'pot'])
+            # print('big_blind:', data[u'big_blind'])
+            # hand2 = print('hand[1]:', data[u'hand'][1]
+
+            print('rank:', hand_rank(data[u'hand']))
 
 
-            print('board:', data[u'board'])
+            # print('board:', data[u'board'])
 
 
             return {'bid': self.__get_bid(data)}
@@ -114,7 +198,7 @@ DevOps Poker Bot - usage instruction
 ------------------------------------
 python3 dplayer.py <team name> <port> <password>
 example:
-    python3 dplayer bazinga 40001 x407
+    python dplayer Devops_Adventurer 40001 eraX1
         """)
         return 0
 
@@ -138,82 +222,3 @@ example:
 if __name__ == '__main__':
     main()
 
-
-def poker(hands):
-    "Return the best hand: poker([hand,...]) => hand"
-    return max(hands, key=hand_rank)
-
-def hand_rank(hand):
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):            # straight flush
-        return (8, max(ranks))
-    elif kind(4, ranks):                           # 4 of a kind
-        return (7, kind(4, ranks), kind(1, ranks))
-    elif kind(3, ranks) and kind(2, ranks):        # full house
-        return (6,kind(3,ranks),kind(2,ranks))
-    elif flush(hand):                              # flush
-        return (5,ranks)
-    elif straight(ranks):                          # straight
-        return (4,max(ranks))
-    elif kind(3, ranks):                           # 3 of a kind
-        return (3,kind(3,ranks),ranks)
-    elif two_pair(ranks):                          # 2 pair
-        return (2,two_pair(ranks),ranks)
-    elif kind(2, ranks):                           # kind
-        return (1,kind(2,ranks),ranks)
-    else:                                          # high card
-        return (0,ranks)
-
-def card_ranks(cards):
-	"Return a list of the ranks,sorted with higher first"
-	ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
-	ranks.sort(reverse = True)
-	return ranks
-
-def straight(ranks):
-	"Return True if the ordered ranks from a 5-card straight"
-	return (max(ranks)-min(ranks) == 4) and len(set(ranks)) ==5
-
-def flush(hand):
-	"Return True if all the cards have the same suit"
-	suits = [s for r,s in hand]
-	return len(set(suits)) == 1
-
-def kind(n,ranks):
-    for r in ranks:
-        if ranks.count(r) == n: return r
-    return None
-
-
-
-def two_pair(ranks):
-    pair = kind(2,ranks)
-    lowpair = kind(2,list(reversed(ranks)))
-    print (pair and lowpair)
-    if pair and lowpair !=pair:
-        return (pair,lowpair)
-    else:
-        return None
-
-		
-def test():
-    "Test cases for the functions in poker program"
-    sf = "6D 7C".split() # Straight Flush
-    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
-    fh = "TD TC TH 7C 7D".split() # Full House
-# result of card_ranks(sf) is [10,9,8,7,6]
-    #assert card_ranks(sf) == [10,9,8,7,6]
-    #assert card_ranks(fk) == [9,9,9,9,7]
-    #assert card_ranks(fh) == [10,10,10,7,7]
-    #print(poker([sf]))
-    print(hand_rank(fk))
-#poker([sf, fk, fh]) is sf
-    #assert poker([sf, fk, fh]) == sf
-    #assert poker([fk, fh]) == fk
-    #assert poker([fh, fh]) == fh
-    #assert poker([sf]) == sf
-    #assert poker([sf] + 99*[fh]) == sf
-    #assert hand_rank(sf) == (8, 10)
-    #assert hand_rank(fk) == (7, 9, 7)
-    #assert hand_rank(fh) == (6, 10, 7)
-    return 'tests pass'		
